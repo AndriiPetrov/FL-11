41c444767a89d5c68a08bae223a936e2523f77c8
AndriiPetrov1 <Bratok3000@gmail.com>
2019-07-05 20:09:13 +0300

    finished 6 homework

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,23 @@
-// Your code goes here
+// Coordinates of 3 points
+let a1 = parseFloat(prompt('Insert your coordinate x of point A', ''));
+let a2 = parseFloat(prompt('Insert your coordinate y of point A', ''));
+
+let b1 = parseFloat(prompt('Insert your coordinate x of point B', ''));
+let b2 = parseFloat(prompt('Insert your coordinate y of point B', ''));
+
+let c1 = parseFloat(prompt('Insert your coodinate x of point C', ''));
+let c2 = parseFloat(prompt('Insert your coordinate y of point C',''));
+
+if( isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
+    console.log(false);
+} else {
+    const Two = 2;
+    let middleCoordinateX = (a1 + b1) / Two;
+    let middleCoordinateY = (a2 + b2) / Two;
+
+    if(middleCoordinateX === c1 && middleCoordinateY === c2 ) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,20 @@
 // Your code goes here
+let firstTriangleSide = parseFloat(prompt('Insert first lenght of Triangle side', ''));
+let secondTriangleSide = parseFloat(prompt('Insert second lenght of Triangle side', ''));
+let thirdTriangleSide = parseFloat(prompt('Insert third lenght of Triangle side', ''));
+
+if(firstTriangleSide + secondTriangleSide < thirdTriangleSide 
+    || firstTriangleSide + thirdTriangleSide < secondTriangleSide 
+    || secondTriangleSide + thirdTriangleSide < firstTriangleSide) {
+        console.log('Triangle doesn\'t exist');
+    } else if ( firstTriangleSide === secondTriangleSide 
+        && thirdTriangleSide === secondTriangleSide 
+        && firstTriangleSide === thirdTriangleSide) {
+        console.log('Equivalent triangle');
+    } else if ( firstTriangleSide === secondTriangleSide 
+        || secondTriangleSide === thirdTriangleSide 
+        || firstTriangleSide === thirdTriangleSide) {
+        console.log('Isosceles triangle');
+    } else {
+        console.log('Normal triangle');
+    }
