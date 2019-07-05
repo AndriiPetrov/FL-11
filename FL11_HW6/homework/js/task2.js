8ca5fe93223f3f77099f3489b822345614541adc
AndriiPetrov1 <Bratok3000@gmail.com>
2019-07-05 20:25:27 +0300

    make better styling for homework6

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,20 +1,26 @@
 // Your code goes here
-let firstTriangleSide = parseFloat(prompt('Insert first lenght of Triangle side', ''));
-let secondTriangleSide = parseFloat(prompt('Insert second lenght of Triangle side', ''));
-let thirdTriangleSide = parseFloat(prompt('Insert third lenght of Triangle side', ''));
+let firstTriangleSide = parseFloat( prompt('Insert first lenght of Triangle side', '') );
+let secondTriangleSide = parseFloat( prompt('Insert second lenght of Triangle side', '') );
+let thirdTriangleSide = parseFloat( prompt('Insert third lenght of Triangle side', '') );
 
-if(firstTriangleSide + secondTriangleSide < thirdTriangleSide 
-    || firstTriangleSide + thirdTriangleSide < secondTriangleSide 
-    || secondTriangleSide + thirdTriangleSide < firstTriangleSide) {
-        console.log('Triangle doesn\'t exist');
-    } else if ( firstTriangleSide === secondTriangleSide 
-        && thirdTriangleSide === secondTriangleSide 
-        && firstTriangleSide === thirdTriangleSide) {
-        console.log('Equivalent triangle');
-    } else if ( firstTriangleSide === secondTriangleSide 
-        || secondTriangleSide === thirdTriangleSide 
-        || firstTriangleSide === thirdTriangleSide) {
-        console.log('Isosceles triangle');
-    } else {
-        console.log('Normal triangle');
-    }
+if (
+    firstTriangleSide + secondTriangleSide < thirdTriangleSide ||
+    firstTriangleSide + thirdTriangleSide < secondTriangleSide ||
+    secondTriangleSide + thirdTriangleSide < firstTriangleSide
+) {
+    console.log('Triangle doesn\'t exist');
+} else if (
+    firstTriangleSide === secondTriangleSide &&
+    thirdTriangleSide === secondTriangleSide && 
+    firstTriangleSide === thirdTriangleSide
+) {
+    console.log('Equivalent triangle');
+} else if (
+    firstTriangleSide === secondTriangleSide ||
+    secondTriangleSide === thirdTriangleSide ||
+    firstTriangleSide === thirdTriangleSide
+) {
+    console.log('Isosceles triangle');
+} else {
+    console.log('Normal triangle');
+}
