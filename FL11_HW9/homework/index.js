d64f70000c5663410c5fd37e760f6e5d0538d113
AndriiPetrov1 <Bratok3000@gmail.com>
2019-07-16 15:40:19 +0300

    exersise 7 done

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -11,15 +11,15 @@ function getNumbers(stringParam) {
   return arrayOfNumbers;
 }
 
-getNumbers('string');
-getNumbers('n1um3ber95');
+//getNumbers('string');
+//getNumbers('n1um3ber95');
 
 // 1.
 function findTypes() {
   let objectOfTypes = new Object();
 
   for (let i = 0; i < arguments.length; i++) {
-    let typeOfProperty = typeof(arguments[i]);
+    let typeOfProperty = typeof arguments[i];
     if (objectOfTypes.hasOwnProperty(typeOfProperty)) {
       objectOfTypes[typeOfProperty] += 1;
     } else {
@@ -30,42 +30,78 @@ function findTypes() {
   return objectOfTypes;
 }
 
-findTypes('number');
-findTypes(null, 5, 'hello');
+//findTypes('number');
+//findTypes(null, 5, 'hello');
 
 // 2.
 function executeforEach(array, executionFunction) {
+  let arrayOfElements = [];
+
   for (let i = 0; i < array.length; i++) {
-    executionFunction(array[i]);
+    if(executionFunction(array[i])) {
+      arrayOfElements.push(array[i]);
+
+      if(array.length - 1 === i) {
+        return arrayOfElements;
+      }
+    }
   }
 }
 
-executeforEach([1,2,3], function(el) { console.log(el) });
+//executeforEach([1,2,3], function(el) { console.log(el) });
 
 // 3.
 function mapArray(array, executionFunction) {
-  executeforEach(array, executionFunction);
+  for (let i = 0; i < array.length; i++) {
+    array[i] = executionFunction(array[i]);
+  }
+
+  return array;
 }
 
-// function executeforEach(array, executionFunction) {
-//   let arrayMap = [];
-//   for (let i = 0; i < array.length; i++) {
-//     if (executionFunction(array[i]) !== 'undefined') {
-//       arrayMap[i] = executionFunction(array[i]);
-//       if(array.length - 1) {
-//         return arrayMap;
-//       }
-//     }
-//   } 
-// }
+//mapArray([2, 5, 8], function(el) { return el + 3 } );
 
-// function mapArray(array, executionFunction1) {
-//   let arrayMap;
-//   arrayMap = executeforEach(array, executionFunction1);
-//   return arrayMap;
-// }
+// 4.
+function filterArray(array, executionFunction) {
+  array = executeforEach(array, executionFunction);
 
-// console.log(executeforEach([1,2,3], function(el) { console.log(el); }))
-// // console.log(mapArray([2, 5, 8], function(el) { return el + 3; }) );
+  return array;
+}
 
-mapArray([2, 5, 8], function(el) { return el + 3 }) 
+//filterArray([2, 5, 8], function(el) { return el > 3 }) 
+
+// 5.
+function showFormattedDate(date) {
+  let startWith = 4;
+  let endWith = 15
+  let dateTime = date.toString().slice(startWith, endWith);
+
+  return `Date: ${dateTime}`;
+}
+
+//showFormattedDate(new Date('2019-01-27T01:10:00'));
+
+// 6.
+function canConvertToDate(date) {
+  let dateTime = new Date(date).toString();
+
+  return dateTime !== 'Invalid Date';
+}
+
+//canConvertToDate(‘2016-13-18T00:00:00’) 
+//canConvertToDate(‘2016-03-18T00:00:00’) 
+
+// 7.
+function daysBetween(date1, date2) {
+  date1 = date1.getTime()
+  date2 = date2.getTime()
+
+  const millisecondsOnMinute = 1000;
+  const secondsOnMinute = 60;
+  const minutesOnHour = 60;
+  const hoursOnDay = 24
+
+  return Math.round((date2 - date1) / millisecondsOnMinute / secondsOnMinute / minutesOnHour / hoursOnDay);
+}
+
+//daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00'))  
