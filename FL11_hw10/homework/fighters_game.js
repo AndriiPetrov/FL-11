2740bfdd7b83653e3a088d613e7ce115a7da0f4d
AndriiPetrov1 <Bratok3000@gmail.com>
2019-07-18 20:42:43 +0300

    lesson10

---
+++ FL11_hw10/homework/fighters_game.js
@@ -0,0 +1,10 @@
+/* Your code goes here */
+class Fighter {
+    constructor(name, damage, hp, agility) {
+        this.name = name;
+        this.damage = damage;
+        this.hp = hp;
+        this.agility = agility;
+    }
+    getName() 
+}
