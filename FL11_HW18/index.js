17fff404286421ba97ee3070bf2935d7daa44c24
Bratok3000 <Bratok3000@gmail.com>
2019-08-28 00:26:25 +0300

    h18

--- FL11_HW18/index.js
+++ FL11_HW18/index.js
@@ -16,7 +16,7 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 
       editButton.addEventListener('click', edit);
       
-      console.log(arrayOfPosts[i]);
+      // console.log(arrayOfPosts[i]);
     }
   });
 
@@ -27,4 +27,34 @@ function edit(e) {
   let input = document.createElement('input');
   input.innerHTML = text;
   li.replaceChild(input, text);
+
+  console.log(e.target);
+  e.target.addEventListener('click', save);
 }
+
+function save(e) {
+  console.log(e.target);
+  console.log(e.target.previousSibling.innerHTML);
+  let input = e.target.previousSibling;
+
+}
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/${id}', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: 1,
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'DELETE'
+})
