5d8678e643f871b0b101057f68d71319b1844e22
AndriiPetrov1 <Bratok3000@gmail.com>
2019-10-01 16:53:41 +0300

    homework last

--- flx_homework_nodejs-expressjs/homework/src/middlewares/delete-authorization.js
+++ flx_homework_nodejs-expressjs/homework/src/middlewares/delete-authorization.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.headers); next();
+})
+
+app.use((req, res, next) => {
+    if( req.header === "X-Password qwerty") next();
+})
+
+app.delete("../../db/data.json", (req, res) => {
+    res.status(401).send("Unauthorized");
+})
+
+app.listen(3000);
--- flx_homework_nodejs-expressjs/homework/src/server.js
+++ flx_homework_nodejs-expressjs/homework/src/server.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+    response.send("Hello from Express!");
+});
+
+app.listen(port, err => {
+    if (err) {
+        return console.log("something bad happend", err);
+    }
+    console.log(`server is istening on ${port}`);
+})
